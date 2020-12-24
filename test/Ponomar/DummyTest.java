246c29195dba7ae0e6fcb88d881ae64311633154
Christoffer G. Thomsen <chris@cgt.name>
2020-12-24 17:26:56 +0100

    Install JUnit 5.6.2

---
+++ test/Ponomar/DummyTest.java
@@ -0,0 +1,12 @@
+package Ponomar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DummyTest {
+    @Test
+    public void junit_is_up_and_running() {
+        assertTrue(true);
+    }
+}
